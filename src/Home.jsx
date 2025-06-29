0833811021b9c3842c2cefa04fa04ec5216c1fad
Dipesh Jha <143820847+djha09@users.noreply.github.com>
2025-06-29 18:43:46 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="homepage">
       <header className="hero">
-        <h1>🛡️ ClearClaim</h1>
+        <h1>🛡️ClearClaim</h1>
         {/* <p>Master Your Policy, Verify Every Detail, Dispute with Confidence, and Get Help in an Instant!</p> */}
         {/* <div className="hero-buttons">
           <a href="/simplify" className="btn">Simplify Policy</a>
@@ -24,17 +24,18 @@ const Home = () => {
         <h2>📌 Our Core Features</h2>
         <div className="feature-cards">
           <div className="card">
+            
             <h3>📄 Simplify Insurance</h3>
             <p>Upload your insurance policy PDF and get an easy-to-read summary in seconds.</p>
             <a href="/simplify" className="card-btn">Simplify →</a>
           </div>
           <div className="card">
-            <h3>✉️ Rebuttal Generator</h3>
+            <h3>📝 Rebuttal Generator</h3>
             <p>Denied a claim? Upload your rejection letter and let AI draft a strong rebuttal.</p>
             <a href="/rebuttal" className="card-btn">Generate →</a>
           </div>
           <div className="card">
-            <h3>✉️ Claim Verifying</h3>
+            <h3>✅ Claim Verifying</h3>
             <p>Unsure about the agent’s explanation? Verify the details and ensure everything adds up</p>
             <a href="/verify" className="card-btn">Verify →</a>
           </div>
